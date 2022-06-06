9da914695f1bd52ea88b88e11e8f87de7fafbd42
Jonathan Ribeiro da Silva <jota.ribeiro.silva@gmail.com>
2022-06-05 22:50:38 -0300

    missionarios_canibais lista 2 - FUNCIONANDO

--- src/missionarios_canibais.py
+++ src/missionarios_canibais.py
@@ -1,4 +1,6 @@
 import copy
+from typing import List
+
 
 class Margem():
     def __init__(self, lado: str, n_missionarios=0, n_canibais=0):
@@ -6,11 +8,23 @@ class Margem():
         self.n_canibais = n_canibais
         self.lado = lado
 
+    def __eq__(self, other):
+        return self.n_missionarios == other.n_missionarios and self.n_canibais == other.n_canibais and self.lado == other.lado
+
 
 class Estado():
-    def __init__(self, margem_esquerda: Margem, margem_direita: Margem):
+    def __init__(self, margem_esquerda: Margem, margem_direita: Margem,
+                 posicao_barco: str):
         self.margem_esquerda = margem_esquerda
         self.margem_direita = margem_direita
+        self.posicao_barco = posicao_barco
+
+    def __eq__(self, other: 'Estado'):
+        if not isinstance(other, Estado): return False
+        return self.margem_esquerda == other.margem_esquerda and self.margem_direita == other.margem_direita and self.posicao_barco == other.posicao_barco
+
+    def __repr__(self):
+        return f'[Margem {self.margem_esquerda.lado} Missionários: {self.margem_esquerda.n_missionarios} Canibais: {self.margem_esquerda.n_canibais}' + f'| Margem {self.margem_direita.lado} Missionários: {self.margem_direita.n_missionarios} Canibais: {self.margem_direita.n_canibais} | Barco: {self.posicao_barco}]\n'
 
 
 class Barco():
@@ -25,22 +39,25 @@ class Barco():
         # já transportou o pessoal, não tem mais o que fazer tio
         if estado.margem_esquerda.n_missionarios == 0 and estado.margem_esquerda.n_canibais == 0:
             return
-
         novo_estado = copy.deepcopy(estado)
         # movimentando o barquinho
-        if self.margem == 'esquerda':
+        if novo_estado.posicao_barco == 'esquerda':
+            if novo_estado.margem_esquerda.n_missionarios < nm or novo_estado.margem_esquerda.n_canibais < nc: return
             self.margem = 'direita'
             novo_estado.margem_esquerda.n_canibais -= nc
             novo_estado.margem_esquerda.n_missionarios -= nm
             novo_estado.margem_direita.n_canibais += nc
             novo_estado.margem_direita.n_missionarios += nm
+            novo_estado.posicao_barco = 'direita'
             # print(f'movimentou da esquerda pra direita com {nm} missionários e {nc} canibais')
         else:
+            if novo_estado.margem_direita.n_missionarios < nm or novo_estado.margem_direita.n_canibais < nc: return
             self.margem = 'esquerda'
             novo_estado.margem_direita.n_canibais -= nc
             novo_estado.margem_direita.n_missionarios -= nm
             novo_estado.margem_esquerda.n_canibais += nc
             novo_estado.margem_esquerda.n_missionarios += nm
+            novo_estado.posicao_barco = 'esquerda'
             # print(f'movimentou da direita pra esquerda com {nm} missionários e {nc} canibais')
         return novo_estado
 
@@ -61,15 +78,19 @@ barco = Barco('esquerda')
 
 def sucessores(estado: Estado):
     sucessores = []
-    for (i, j) in possibilidades_barco:
-        novo_estado = barco.deslocar(estado, i, j)
+    global barco
+
+    for (m, c) in possibilidades_barco:
+        barco_copy = copy.deepcopy(barco)
+        novo_estado = barco_copy.deslocar(estado, m, c)
         if novo_estado is None: continue
         # não pode haver menos missionários do canibais em qualquer uma  das margens
         if (
                 novo_estado.margem_esquerda.n_missionarios < novo_estado.margem_esquerda.n_canibais and novo_estado.margem_esquerda.n_missionarios > 0) or (
                 novo_estado.margem_direita.n_missionarios < novo_estado.margem_direita.n_canibais and novo_estado.margem_direita.n_missionarios > 0):
             continue
-        if novo_estado in visitados: continue
+        if novo_estado in visitados:
+            continue
         sucessores.append(novo_estado)
     return sucessores
 
@@ -81,17 +102,19 @@ def adjacenteNaoVisitado(estado: Estado):
     else:
         return -1
 
+
 # Verifica se o objetivo foi atingido
 def testeObjetivo(estado: Estado, n_missionarios, n_canibais):
     return estado.margem_direita.n_missionarios == n_missionarios and estado.margem_direita.n_canibais == n_canibais
 
 
-def dfs(estadoInicial : Estado):
+def dfs(estadoInicial: Estado):
     borda.append(estadoInicial)
     while len(borda) != 0:
         estado = borda[len(borda) - 1]
         # se atingiu o objetivo, blz, pode parar
-        if testeObjetivo(estado, estadoInicial.margem_esquerda.n_missionarios, estadoInicial.margem_esquerda.n_canibais): break
+        if testeObjetivo(estado, estadoInicial.margem_esquerda.n_missionarios,
+                         estadoInicial.margem_esquerda.n_canibais): break
         v = adjacenteNaoVisitado(estado)
         if v == -1:
             borda.pop()
@@ -106,10 +129,10 @@ def dfs(estadoInicial : Estado):
 def main():
     margem_esquerda = Margem('esquerda', 3, 3)
     margem_direita = Margem('direita', 0, 0)
-    estadoInicial = Estado(margem_esquerda, margem_direita)
-    solucao = dfs(estadoInicial)
-    print(solucao)
-
+    estadoInicial = Estado(margem_esquerda, margem_direita, 'esquerda')
+    visitados.append(estadoInicial)
+    solucao: List[Estado] = dfs(estadoInicial)
+    print(str(solucao))
 
 
 if __name__ == '__main__':
